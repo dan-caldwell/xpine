3896f674754171e949b0815571e885b91ff265d8
dan-caldwell <dan@caldwell.info>
2025-03-22 20:32:55 -0400

    fix bug in getXPineDistDir

--- src/util/require.ts
+++ src/util/require.ts
@@ -4,6 +4,6 @@ export default createRequire(import.meta.url);
 
 export function getXPineDistDir() {
   const dir = import.meta.dirname;
-  const splitDir = dir.split('/xpine/dist/');
+  const splitDir = dir.split('/xpine/dist');
   return splitDir[0] + '/xpine/dist';
 }
