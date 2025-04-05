b6d8b5403db814555b96c64be25135f84de2e3f3
dan-caldwell <dan@caldwell.info>
2025-04-05 19:39:38 -0400

    add tests for spa and non-spa functionality

--- tests/e2e/spa.spec.ts
+++ tests/e2e/spa.spec.ts
@@ -3,19 +3,21 @@ import { url } from '../playwright.config';
 
 
 test('sticky nav creates request for page contents and does not reload page', async ({ page }) => {
-  page.on('load', () => console.log('Page loaded!'));
   await page.goto(url);
   const link = page.getByTestId('navbar-boolean-static-path');
+  const navbarNow = await page.getByTestId('navbar-now').getAttribute('data-now');
   await link.click();
   await page.waitForURL('**/boolean-static-path');
-  await expect(page.getByTestId('navbar-boolean-static-path')).toBeAttached();
+  const newNavbarNow = await page.getByTestId('navbar-now').getAttribute('data-now');
+  expect(navbarNow).toEqual(newNavbarNow);
 });
 
 test('non-spa request for page contents reloads page', async ({ page }) => {
   await page.goto(url);
-  const link = page.getByTestId('navbar-path-d-non-spa');
-  page.on('load', () => console.log('Page loaded!'));
+  const link = page.getByTestId('navbar-non-static-path-non-spa');
+  const navbarNow = await page.getByTestId('navbar-now').getAttribute('data-now');
   await link.click();
-  await page.waitForURL('**/my-path-a2/my-path-b2/my-path-c2/2');
-  expect(await link.count()).toEqual(0);
+  await page.waitForURL('**/base-static-path/non-static-path');
+  const newNavbarNow = await page.getByTestId('navbar-now').getAttribute('data-now');
+  expect(navbarNow).not.toEqual(newNavbarNow);
 });
